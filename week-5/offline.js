11f27c5efe7d3c4df6dbcc9a53a281b2efe9896f
Aaidt <aadits2004@gmail.com>
2025-01-27 21:34:08 +0530

    Axios vs Fetch
    Using HTTP dump for testing

--- week-5/offline.js
+++ week-5/offline.js
@@ -5,7 +5,7 @@
 // const answer = sum(1,2);
 // console.log(answer);
 
-const arr = [1, 2, 3, 4];
+// const arr = [1, 2, 3, 4];
 
 // const newArray = []
 
@@ -20,5 +20,26 @@ const arr = [1, 2, 3, 4];
 // console.log(ans)
 
 
-const ans = arr.filter((i) => i%2==0);
-console.log(ans)
+// const ans = arr.filter((i) => i%2==0);
+// console.log(ans)
+
+
+// const axios = require("axios");
+
+// async function fetchingFromHTTPDump(){
+//     const response = await axios.get(
+//         " https://httpdump.app/dumps/791fc7c0-db18-45eb-bee0-c32c87339d8b",{
+//         headers: {
+//             "message": "hey hope you have a good day..."
+//         },
+//         body: {
+//             msg: "i hate vs code prompts, they suck ass"
+//         }
+//     });
+
+// console.log(response.data);
+
+// } 
+
+
+// fetchingFromHTTPDump();
