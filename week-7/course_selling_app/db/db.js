74d237bca89b88e7bd1b5adad228a38e06a306fa
Aaidt <aadits2004@gmail.com>
2025-02-15 16:38:20 +0530

    schema defined.

---
+++ week-7/course_selling_app/db/db.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.ObjectId;
+const Schema = mongoose.Schema;
+
+
+const User = new Schema({
+    email: String,
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+
+const Course = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageURL: String,
+    creatorId: ObjectId
+});
+
+const Admin = new Schema({
+    email: String,
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const Purchase = new Schema({
+    courseId: ObjectId,
+    userId: ObjectId
+});
+
+
+const UserModel = mongoose.model("users", User);
+const CourseModel = mongoose.model("course", Course);
+const AdminModel = mongoose.model("Admin", Admin);
+const PurchaseModel = mongoose.model("Purchase", Purchase);
+
+
+module.exports = {
+    UserModel: UserModel,
+    CourseModel: CourseModel,
+    AdminModel: AdminModel,
+    PurchaseModel: PurchaseModel  
+}
