6353ba9f8259882d28e84b705922d5e4a3109023
Aaidt <aadits2004@gmail.com>
2025-02-27 19:14:44 +0530

    added auth endpoints

--- week-7/course_selling_app/index.js
+++ week-7/course_selling_app/index.js
@@ -6,7 +6,7 @@ const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/courses");
 const { adminRouter } = require("./routes/admin");
 
-app.use(express.json());
+app.use(express.json());    
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
--- week-7/course_selling_app/routes/admin.js
+++ week-7/course_selling_app/routes/admin.js
@@ -1,18 +1,43 @@
 const { Router } = require("express");
 const adminRouter = Router();
-const { adminModel } = require("../db/db");
+const { AdminModel } = require("../db/db");
+const JWT_ADMIN_PASSWORD = "adasdsadsadsdsadsdas";
+const jwt = require("jsonwebtoken");
 
 
-adminRouter.post("/signup", function(req, res){
+adminRouter.post("/signup", async function(req, res){
+    const { email, password, firstName, lastName } = req.body; // add zod validation
+    // hash the password so the original is not saved in the db
+    await AdminModel.create({
+        email,
+        password,
+        firstName,
+        lastName
+    });
     res.json({
         message: "signup endpoint."
     });
 })
 
-adminRouter.post("/signin", function(req, res){
-    res.json({
-        message: "signin endpoint."
+adminRouter.post("/signin", async function(req, res){
+    const { email, password } = req.body;
+    const admin = await AdminModel.findOne({    
+        email,
+        password
     });
+
+    if(admin){
+        const token = jwt.sign({
+            id: admin._id    
+        }, JWT_ADMIN_PASSWORD);
+        res.json({
+            token
+        });
+    }else{
+        res.json({
+            message: "incorrect credentials"
+        });
+    }
 })
 
 adminRouter.get("/courses/bulk", function(req, res){
