01898f205be263ff7d6f0f40e8bce90d413ca69e
Aaidt <aadits2004@gmail.com>
2025-03-01 13:36:05 +0530

    added input validation using zod

--- week-7/e-commerce/routes/admin.js
+++ week-7/e-commerce/routes/admin.js
@@ -2,13 +2,30 @@ const { Router } = require("express");
 const { mongoose } = require("mongoose");
 const { AdminModel } = require("../db/db");
 
+const { z } = require("zod");
+
 const adminRouter = Router();
 
 adminRouter.post("/signup", async function(req, res) {
-    const { username, password, firstName, lastName } = req.body;
     // input validation using zod
+    const Admin = z.object({
+        username: z.string().min(11).max(70).email(),
+        password: z.string().min(8, { message: "Must be 8 characters or longer." }).max(70, { message: "Must be max 70 characters" }),
+        firstName: z.string().min(3, { message: "Must be 3 characters or longer." }).max(70, { message: "Must be max 70 characters" }),
+        lastName: z.string().min(3, { message: "Must be 3 characters or longer." }).max(70, { message: "Must be max 70 characters" }),
+    });
 
+    const data = Admin.safeParse(req.body);
 
+    if(!data.success){
+        res.status(403).json({
+            message: "Incorrect format.",
+            error: data.error
+        });
+        return
+    }
+    
+    const { username, password, firstName, lastName } = req.body;
     // hash the password
 
 
