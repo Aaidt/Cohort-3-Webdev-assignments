8a07004b1d55ef1399f6df530f0d80adcf7a194b
Aaidt <aadits2004@gmail.com>
2025-03-01 11:42:47 +0530

    added the main index file with routing

--- week-7/e-commerce/index.js
+++ week-7/e-commerce/index.js
@@ -1,2 +1,25 @@
+require("dotenv").config();
 const { express } = require("express");
+const app = express();
+const mongoose  = require("mongoose");
 
+const { userRouter } = require("../routes/user");
+const { adminRouter } = require("../routes/admin");
+const { itemRouter } = require("../routes/item");
+
+const { port } = require("./config");
+
+app.use(express.json());
+
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/item", itemRouter);
+
+async function main(){
+    await mongoose.connect(process.env.MONGODB_URL);
+    app.listen(process.env.PORT, () => {
+        console.log(`The server is listening on port: ${process.env.PORT}`);
+    });
+}
+
+main();
