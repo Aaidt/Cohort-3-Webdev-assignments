2d5e9e7e96b3dc94403c180d6602efe2eb32a5c4
Aaidt <aadits2004@gmail.com>
2025-03-03 16:21:49 +0530

    useEffect hook for mounting

--- week-8/vite-project/src/App.jsx
+++ week-8/vite-project/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Counter></Counter>
+    </div>
   )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  console.log("counter");
+
+  useEffect(function(){
+    setInterval(function (){
+      setCount(count => count + 1);
+    }, 1000)
+    console.log("mounted");
+  }, []);
+
+
+  // function increaseCount(){
+  //   setCount(count + 1);
+  // }
+  // function decreaseCount(){
+  //   setCount(count - 1);
+  // }
+  // function resetCount(){
+  //   setCount(0);
+  // }
+
+  return <div>
+    <h2>{count}</h2>
+    {/* <button onClick={increaseCount}>Increase Count</button>
+    <button onClick={decreaseCount}>Decrease Count</button>
+    <button onClick={resetCount}>Reset Count</button> */}
+  </div>
 }
 
 export default App
