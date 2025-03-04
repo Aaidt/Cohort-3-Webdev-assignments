f248261c38f1540dc2f062ed6422a51ef20137c6
Aaidt <aadits2004@gmail.com>
2025-03-04 20:26:31 +0530

    post component.

--- week-8/vite-project/lec-work/src/App.jsx
+++ week-8/vite-project/lec-work/src/App.jsx
@@ -2,31 +2,43 @@
 
 function App(){
   return (
-      <div>
-        <PostComponent /> 
+      <div style={{background: "#dfe6e9", height: "100vh"}}>
+        <div style={{display: "flex", justifyContent: "center"}}>
+          <div>
+            <div>
+              <PostComponent />  
+            </div> <br />
+            <div>
+              <PostComponent />  
+            </div> <br />
+            <div>
+              <PostComponent />  
+            </div> <br />
+          </div>
+        </div> 
       </div>   
       
   )
 } 
 
-const style = {width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, display: "flex"}
+const style = {width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, padding: 20}
 
 function PostComponent(){
   return (
-  <div> 
-    <div style={style}>
+  <div style={style}> 
+    <div style={{display: "flex", }}>
       <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{
-          height: 20,
-          width: 20,
+          height: 30,
+          width: 30,
           borderRadius: 20
       }} />
-      <div style={{fontSize: 10}}>
+      <div style={{fontSize: 10, marginLeft: 10,}}>
         <b>100x devs</b>
         <div>23,000 followers</div>
         <div>12m</div>
       </div>
     </div>
-    <div style={{fontSize: 12  }}>Want to know how to win big!!!</div>
+    <div style={{fontSize: 12  }}>Want to know how to win big!!! Discover how these kids earned $5000 in this hackathon</div>
   </div>
   )
 }
