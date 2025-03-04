a568e6c2b45f3022c2756724b21ea51b6fc931e3
Aaidt <aadits2004@gmail.com>
2025-03-04 21:58:02 +0530

    props and conditionally renedering the timer

--- week-8/vite-project/lec-work/src/App.jsx
+++ week-8/vite-project/lec-work/src/App.jsx
@@ -6,13 +6,28 @@ function App(){
         <div style={{display: "flex", justifyContent: "center"}}>
           <div>
             <div>
-              <PostComponent />  
+              <PostComponent 
+                image={"https://imgs.search.brave.com/ejFC42IMOZGFQeZ3QIcoXxMqow_oT-L61P7mhwwkeJI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcHB4/LXdzYi1nY3AuYWth/bWFpLm5ldC5pbi9z/dWJqZWN0LzIwMjMt/MDEtMTctMC4xNzA0/NDM2MDEyMDk1MTE4/NS5qcGc"}
+                name={"100x devs"}
+                subtitles={"23,000 followers"}
+                time={"12m"}
+              />  
             </div> <br />
             <div>
-              <PostComponent />  
+              <PostComponent
+                image={"https://imgs.search.brave.com/ejFC42IMOZGFQeZ3QIcoXxMqow_oT-L61P7mhwwkeJI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcHB4/LXdzYi1nY3AuYWth/bWFpLm5ldC5pbi9z/dWJqZWN0LzIwMjMt/MDEtMTctMC4xNzA0/NDM2MDEyMDk1MTE4/NS5qcGc"}
+                name={"Only 10x losers here"}
+                subtitles={"1,000 followers"}
+                time={"11m"}
+              />  
             </div> <br />
             <div>
-              <PostComponent />  
+              <PostComponent
+                image={"https://imgs.search.brave.com/ejFC42IMOZGFQeZ3QIcoXxMqow_oT-L61P7mhwwkeJI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcHB4/LXdzYi1nY3AuYWth/bWFpLm5ldC5pbi9z/dWJqZWN0LzIwMjMt/MDEtMTctMC4xNzA0/NDM2MDEyMDk1MTE4/NS5qcGc"}
+                name={"Resume making cohort"}
+                subtitles={"Promoted"}
+                time={""} 
+              />  
             </div> <br />
           </div>
         </div> 
@@ -23,19 +38,28 @@ function App(){
 
 const style = {width: 200, backgroundColor: "white", borderRadius: 10, borderColor: "gray", borderWidth: 1, padding: 20}
 
-function PostComponent(){
+function PostComponent({image, name, subtitles, time}){
   return (
   <div style={style}> 
     <div style={{display: "flex", }}>
-      <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{
+      <img src={image} style={{
           height: 30,
           width: 30,
           borderRadius: 20
       }} />
       <div style={{fontSize: 10, marginLeft: 10,}}>
-        <b>100x devs</b>
-        <div>23,000 followers</div>
-        <div>12m</div>
+        <b>
+          {name}
+        </b>
+        <div>
+          {subtitles}
+        </div>
+        {time ? <div style={{display: "flex"}}>
+          <div>
+            {time}
+          </div>
+          <img style={{height: 10, width: 15}} src={"https://imgs.search.brave.com/3uYiSzoKJNiEe-AQhex44AWZJpV1gbccLs70MB7Wa2A/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTg1/MzA2MzY0L3Bob3Rv/L3RocmVlLW9jbG9j/ay5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9WW9tZF9ESXM1/b0gtcTRhd25YNjJi/WVA5RlBEMzNNNFBR/bDVyOXQtQ1k5Yz0"} />
+        </div> : null}
       </div>
     </div>
     <div style={{fontSize: 12  }}>Want to know how to win big!!! Discover how these kids earned $5000 in this hackathon</div>
@@ -43,5 +67,11 @@ function PostComponent(){
   )
 }
 
+
+// function profileCard(){
+//   return <div>
+
+//   </div>
+// }
 
 export default App
