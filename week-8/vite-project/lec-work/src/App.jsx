c271900ccbd84c231a9004e57b1fd63654367290
Aaidt <aadits2004@gmail.com>
2025-03-05 09:45:11 +0530

    iterating through posts.

--- week-8/vite-project/lec-work/src/App.jsx
+++ week-8/vite-project/lec-work/src/App.jsx
@@ -2,18 +2,36 @@
 import { PostComponent } from "./post";
 
 function App(){
+
+  const post = [{
+    name: "100x devs",
+    subtitle: "23,000 followers",
+    time: "12m",
+    image: "https://imgs.search.brave.com/ejFC42IMOZGFQeZ3QIcoXxMqow_oT-L61P7mhwwkeJI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcHB4/LXdzYi1nY3AuYWth/bWFpLm5ldC5pbi9z/dWJqZWN0LzIwMjMt/MDEtMTctMC4xNzA0/NDM2MDEyMDk1MTE4/NS5qcGc",
+    description: "Get rich coding!!!"
+  }];
+
+  const postComponents = post.map(post => <PostComponent
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.time}
+    image={post.image}
+    description={post.description}
+    />
+  )
+
+
+  function addPost(){
+    
+  }
+
   return (
       <div style={{background: "#dfe6e9", height: "100vh"}}>
+        <button onClick={addPost()}>Post something...</button>
         <div style={{display: "flex", justifyContent: "center"}}>
           <div>
-            <div>
-              <PostComponent 
-                name={"100x devs"}
-                subtitle={"23,000 followers"}
-                time={"12m"}
-                image={"https://imgs.search.brave.com/ejFC42IMOZGFQeZ3QIcoXxMqow_oT-L61P7mhwwkeJI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcHB4/LXdzYi1nY3AuYWth/bWFpLm5ldC5pbi9z/dWJqZWN0LzIwMjMt/MDEtMTctMC4xNzA0/NDM2MDEyMDk1MTE4/NS5qcGc"}
-                description={"Get rich coding!!!"}
-              />  
+            <div> 
+                {postComponents} 
             </div> <br />
           </div>
         </div> 
