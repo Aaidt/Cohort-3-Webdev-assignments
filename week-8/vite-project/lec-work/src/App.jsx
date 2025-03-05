35a9c910bd0c9a9233775b2778bbb07b83c929b0
Aaidt <aadits2004@gmail.com>
2025-03-05 19:04:14 +0530

    useEffect

--- week-8/vite-project/lec-work/src/App.jsx
+++ week-8/vite-project/lec-work/src/App.jsx
@@ -1,42 +1,59 @@
 // import './App.css'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PostComponent } from "./post";
 
 function App(){
-  const [post, setPost] = useState([]);
+  // const [post, setPost] = useState([]);
 
-  const postComponents = post.map(post => <PostComponent
-    name={post.name}
-    subtitle={post.subtitle}
-    time={post.time}
-    image={post.image}
-    description={post.description}
-    /> 
-  )
+  // const postComponents = post.map(post => <PostComponent
+  //   name={post.name}
+  //   subtitle={post.subtitle}
+  //   time={post.time}
+  //   image={post.image}
+  //   description={post.description}
+  //   /> 
+  // )
 
-  function addPost(){
-    setPost([...post, {
-      name: "100x devs",
-      subtitle: "23,000 followers",
-      time: "12m",
-      image: "https://imgs.search.brave.com/ejFC42IMOZGFQeZ3QIcoXxMqow_oT-L61P7mhwwkeJI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcHB4/LXdzYi1nY3AuYWth/bWFpLm5ldC5pbi9z/dWJqZWN0LzIwMjMt/MDEtMTctMC4xNzA0/NDM2MDEyMDk1MTE4/NS5qcGc",
-      description: "Get rich coding!!!"
-    }]);    
+  // function addPost(){
+  //   setPost([...post, {
+  //     name: "100x devs",
+  //     subtitle: "23,000 followers",
+  //     time: "12m",
+  //     image: "https://imgs.search.brave.com/ejFC42IMOZGFQeZ3QIcoXxMqow_oT-L61P7mhwwkeJI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcHB4/LXdzYi1nY3AuYWth/bWFpLm5ldC5pbi9z/dWJqZWN0LzIwMjMt/MDEtMTctMC4xNzA0/NDM2MDEyMDk1MTE4/NS5qcGc",
+  //     description: "Get rich coding!!!"
+  //   }]);    
+  // }
+
+  // return (
+  //     <div style={{background: "#dfe6e9", height: "100vh"}}>
+  //       <button onClick={addPost}>Post something...</button>
+  //       <div style={{display: "flex", justifyContent: "center"}}>
+  //         <div>
+  //           <div> 
+  //               {postComponents} 
+  //           </div> <br />
+  //         </div>
+  //       </div> 
+  //     </div>   
+      
+  // )
+
+  const [count, setCount] = useState (0);
+
+
+  useEffect(function (){
+    setInterval(increaseCount, 1000); 
+  }, []);
+  function increaseCount(){
+    setCount(count => count + 1);
   }
 
-  return (
-      <div style={{background: "#dfe6e9", height: "100vh"}}>
-        <button onClick={addPost}>Post something...</button>
-        <div style={{display: "flex", justifyContent: "center"}}>
-          <div>
-            <div> 
-                {postComponents} 
-            </div> <br />
-          </div>
-        </div> 
-      </div>   
-      
-  )
+
+  return <div>
+    {count}
+  </div>
+
+
 } 
 
 // function profileCard(){
