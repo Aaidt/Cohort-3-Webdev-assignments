426c9b91c12cc495f9862f3883ba4f8f57aefa04
Aaidt <aadits2004@gmail.com>
2025-03-05 10:14:10 +0530

    setpost

--- week-8/vite-project/lec-work/src/App.jsx
+++ week-8/vite-project/lec-work/src/App.jsx
@@ -1,15 +1,9 @@
 // import './App.css'
+import { useState } from "react";
 import { PostComponent } from "./post";
 
 function App(){
-
-  const post = [{
-    name: "100x devs",
-    subtitle: "23,000 followers",
-    time: "12m",
-    image: "https://imgs.search.brave.com/ejFC42IMOZGFQeZ3QIcoXxMqow_oT-L61P7mhwwkeJI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcHB4/LXdzYi1nY3AuYWth/bWFpLm5ldC5pbi9z/dWJqZWN0LzIwMjMt/MDEtMTctMC4xNzA0/NDM2MDEyMDk1MTE4/NS5qcGc",
-    description: "Get rich coding!!!"
-  }];
+  const [post, setPost] = useState([]);
 
   const postComponents = post.map(post => <PostComponent
     name={post.name}
@@ -17,17 +11,22 @@ function App(){
     time={post.time}
     image={post.image}
     description={post.description}
-    />
+    /> 
   )
 
-
   function addPost(){
-    
+    setPost([...post, {
+      name: "100x devs",
+      subtitle: "23,000 followers",
+      time: "12m",
+      image: "https://imgs.search.brave.com/ejFC42IMOZGFQeZ3QIcoXxMqow_oT-L61P7mhwwkeJI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9hcHB4/LXdzYi1nY3AuYWth/bWFpLm5ldC5pbi9z/dWJqZWN0LzIwMjMt/MDEtMTctMC4xNzA0/NDM2MDEyMDk1MTE4/NS5qcGc",
+      description: "Get rich coding!!!"
+    }]);    
   }
 
   return (
       <div style={{background: "#dfe6e9", height: "100vh"}}>
-        <button onClick={addPost()}>Post something...</button>
+        <button onClick={addPost}>Post something...</button>
         <div style={{display: "flex", justifyContent: "center"}}>
           <div>
             <div> 
