718a51c98d89378b790d9e69a739d38698602fa5
Aaidt <aadits2004@gmail.com>
2025-03-07 21:26:41 +0530

    added some elements to the form

--- week-9/petAdoption/src/components/Header.jsx
+++ week-9/petAdoption/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div style={{backgroundColor: "rgba(207, 142, 44, 0.5)", padding: 20}}>
-      <b>PET ADOPTION FORM</b>
+      <b>PET ADOPTION FORM</b>  
     </div>
   )
 }
--- week-9/petAdoption/src/components/PetAdoptionForm.jsx
+++ week-9/petAdoption/src/components/PetAdoptionForm.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import "../myApp.css";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const PetAdoptionForm = () => {
+  const tableOutline = {
+    backgroundColor: "rgba(207, 142, 44, 0.5)",
+    marginRight: "200px", 
+    marginLeft: "200px",
+    height: "100vh" 
+  }
+
   return (
-    <div>PetAdoptionForm</div>
+    <div style={tableOutline}>
+      
+      <div>
+        <b>
+          Pet Name:
+        </b>
+        <input style={{margin: 20}} type="text" placeholder="Enter name of the pet:"></input>
+      </div>
+
+      <div>
+        <b>
+          Pet Type:
+        </b>
+        <Dropdown>
+          <Dropdown.toggle variant="success" id="dropdown-basic">
+            Pet Types
+          </Dropdown.toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#action1">Dog</Dropdown.Item>
+            <Dropdown.Item href="#action2">Cat</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
+      <div>
+        <b>
+          Pet Name:
+        </b>
+        <input style={{margin: 20}} type="text" placeholder="Enter name of the pet:"></input>
+      </div>
+
+      <div>
+        <b>
+          Pet Name:
+        </b>
+        <input style={{margin: 20}} type="text" placeholder="Enter name of the pet:"></input>
+      </div>
+
+      <div>
+        <b>
+          Pet Name:
+        </b>
+        <input style={{margin: 20}} type="text" placeholder="Enter name of the pet:"></input>
+      </div>
+    </div>
   )
 }
 
