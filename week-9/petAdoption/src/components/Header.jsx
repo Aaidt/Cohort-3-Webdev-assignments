7bf90ecd8aa031aeea086129a80ebf8c7e3584b0
Aaidt <aadits2004@gmail.com>
2025-03-07 20:48:15 +0530

    header for the adoption form created.

--- week-9/petAdoption/src/components/Header.jsx
+++ week-9/petAdoption/src/components/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div style={{backgroundColor: "rgba(207, 142, 44, 0.5)", padding: 20}}>
+      <b>PET ADOPTION FORM</b>
+    </div>
   )
 }
 
