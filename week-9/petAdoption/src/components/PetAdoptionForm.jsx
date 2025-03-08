e5026b109ebe20975ad7b61675c56cb83c15b718
Aaidt <aadits2004@gmail.com>
2025-03-08 11:52:50 +0530

    handling change, proper forms.

--- week-9/petAdoption/src/components/PetAdoptionForm.jsx
+++ week-9/petAdoption/src/components/PetAdoptionForm.jsx
@@ -4,56 +4,97 @@ import "../myApp.css";
 
 const PetAdoptionForm = () => {
 
-  return (
-    <>
-      <div className="form">
-        
-        <div>
-          <label>
-            Pet Name:
-          </label>
-          <input type="text" placeholder="Enter name of the pet:"></input>
-        </div>
+  const [formData, setFormData] = useState([]);
+  const[values, setValues] = useState({
+    petName:"",
+    petType:"Dog",
+    breed:"",
+    adopterName:"",
+    email:"",
+    phone:""
+  })
 
-        <div>
-          <label>
-            Pet Type:
-          </label>
+  const { petName, petType, breed, adopterName, email, phone } = values;
+
+  const [show, setShow] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setValues({
+      ...values,
+      [name]:value
+    })
+  } 
+
+  if(!show){
+    return (
+      <>
+        <div className="form">
+          
           <div>
-            
+            <label htmlFor="petName">Pet Name:</label>
+            <input 
+              type="text" 
+              placeholder="Enter name of the pet:"
+              name="petName"
+              value={petName}
+              onChange={handleChange}>
+            </input>
           </div>
-        </div>
+  
+          <div>
+            <label htmlFor="petType">Pet Type:</label>
+          </div>
+  
+          <div>
+            <label htmlFor="breed">Breed:</label>
+            <input 
+              type="text" 
+              placeholder="Enter breed:"
+              value={breed}
+              name="breed"
+              onChange={handleChange}>
+            </input>
+          </div>
+  
+          <div>
+            <label htmlFor="adopterName">Your name:</label>
+            <input 
+              type="text" 
+              placeholder="Enter name of the adopter:"
+              value={adopterName}
+              name="adopterName"
+              onChange={handleChange}>
+            </input>
+          </div>
+  
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input 
+              type="text" 
+              placeholder="Enter email:"
+              value={email}
+              name="email"
+              onChange={handleChange}>
+            </input>
+          </div>
+  
+          <div>
+            <label htmlFor="phone">Phone:</label>
+            <input 
+              type="text" 
+              placeholder="Enter phone number:"
+              value={phone}
+              name="phone"
+              onChange={handleChange}>
+            </input>
+          </div>
 
-        <div>
-          <label>
-            Breed:
-          </label>
-          <input type="text" placeholder="Enter name of the pet:"></input>
         </div>
-
-        <div>
-          <label>
-            Your name:
-          </label>
-          <input type="text" placeholder="Enter name of the pet:"></input>
-        </div>
-
-        <div>
-          <label>
-            Email:
-          </label>
-          <input type="text" placeholder="Enter name of the pet:"></input>
-        </div>
-
-        <div>
-          <label>
-            Phone:
-          </label>
-          <input type="text" placeholder="Enter name of the pet:"></input>
-        </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
+  
 }
 
 export default PetAdoptionForm
