91c6515fd00aa47f16ce48a9a0149e99b2d93662
Aaidt <aadits2004@gmail.com>
2025-03-08 10:53:56 +0530

    basic form.

--- week-9/petAdoption/src/components/PetAdoptionForm.jsx
+++ week-9/petAdoption/src/components/PetAdoptionForm.jsx
@@ -1,61 +1,83 @@
 import React from 'react'
+import { useState } from "react";
 import "../myApp.css";
-import Dropdown from 'react-bootstrap/Dropdown';
 
 const PetAdoptionForm = () => {
-  const tableOutline = {
-    backgroundColor: "rgba(207, 142, 44, 0.5)",
-    marginRight: "200px", 
-    marginLeft: "200px",
-    height: "100vh" 
-  }
+// const DropDown = () => {
+//   const [open, setOpen] = useState(false);
+//   const [value, setValue] = useState("");
+
+//   const toggleOpen = () => {
+//     setOpen(!open);
+//   }
+
+//   const handleChange = (event) => {
+//     setValue(event.target.value);
+//   }
+
+//   return (
+//     <div style={{display: "flex"}}>
+//       <input type="text" value={value} onChange={handleChange}></input>
+//       <span><button style={{borderRadius: "10px", margin: 10, padding: 7}} onClick={toggleOpen}>{open ? "isOpen" : "not Open"}</button></span>
+//       {open && (
+//         <ul>
+//           <li onClick={() => setValue("dog")}>DOG</li>
+//           <li onClick={() => setValue("cat")}>CAT</li>
+//         </ul>
+//       )}
+//     </div>
+// )
+// }
 
   return (
-    <div style={tableOutline}>
-      
-      <div>
-        <b>
-          Pet Name:
-        </b>
-        <input style={{margin: 20}} type="text" placeholder="Enter name of the pet:"></input>
-      </div>
+    <>
+      <div className="form">
+        
+        <div>
+          <b>
+            Pet Name:
+          </b>
+          <input style={{margin: 20, width: 900}} type="text" placeholder="Enter name of the pet:"></input>
+        </div>
 
-      <div>
-        <b>
-          Pet Type:
-        </b>
-        <Dropdown>
-          <Dropdown.toggle variant="success" id="dropdown-basic">
-            Pet Types
-          </Dropdown.toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item href="#action1">Dog</Dropdown.Item>
-            <Dropdown.Item href="#action2">Cat</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
+        <div>
+          <b>
+            Pet Type:
+          </b>
+          <div>
+            <DropDown />
+          </div>
+        </div>
 
-      <div>
-        <b>
-          Pet Name:
-        </b>
-        <input style={{margin: 20}} type="text" placeholder="Enter name of the pet:"></input>
-      </div>
+        <div>
+          <b>
+            Breed:
+          </b>
+          <input style={{margin: 20, width: 900}} type="text" placeholder="Enter name of the pet:"></input>
+        </div>
 
-      <div>
-        <b>
-          Pet Name:
-        </b>
-        <input style={{margin: 20}} type="text" placeholder="Enter name of the pet:"></input>
-      </div>
+        <div>
+          <b>
+            Your name:
+          </b>
+          <input style={{margin: 20, width: 900}} type="text" placeholder="Enter name of the pet:"></input>
+        </div>
 
-      <div>
-        <b>
-          Pet Name:
-        </b>
-        <input style={{margin: 20}} type="text" placeholder="Enter name of the pet:"></input>
+        <div>
+          <b>
+            Email:
+          </b>
+          <input style={{margin: 20, width: 900}} type="text" placeholder="Enter name of the pet:"></input>
+        </div>
+
+        <div>
+          <b>
+            Phone:
+          </b>
+          <input style={{margin: 20, width: 900}} type="text" placeholder="Enter name of the pet:"></input>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
