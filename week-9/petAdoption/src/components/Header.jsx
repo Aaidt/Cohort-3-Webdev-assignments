da7e11d10e09ee81d271ef1eba4d7cedc6788fc8
Aaidt <aadits2004@gmail.com>
2025-03-08 15:22:21 +0530

    used props for passing the header.

--- week-9/petAdoption/src/components/Header.jsx
+++ week-9/petAdoption/src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ message }) => {
   return (
     <div style={{backgroundColor: "rgba(207, 142, 44, 0.5)", padding: 20}}>
-      <b>PET ADOPTION FORM</b>  
+      <b>{ message }</b>  
     </div>
   )
 }
