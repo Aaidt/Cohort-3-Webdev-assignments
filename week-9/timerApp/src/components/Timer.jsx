9c226e01eed6c578fdcf73ddc5190c39157cbd25
Aaidt <aadits2004@gmail.com>
2025-03-10 21:11:32 +0530

    started the timer app assignment. buttons created.

--- week-9/timerApp/src/components/Timer.jsx
+++ week-9/timerApp/src/components/Timer.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Timer.module.css" 
+
 
 const Timer = () => {
+
+  const [Time, setTime] = useState("00");
+  const [TimerState, setTimerState] = useState(false);
+
+  const startTimer = () => {
+    const start = setInterval(setTime(
+      time => time =+ 1
+    ), 1000);
+  }
+
+  const stopTimer = (start) => {
+    clearInterval(start)
+  }
+
+
+
   return (
-    <div>Timer</div>
+    <div className="timerApp">
+      Timer
+      <button onClick={startTimer}>START</button>
+      <button onClick={stopTimer}>STOP</button>
+    </div>
   )
 }
+
+
+
 
 export default Timer
