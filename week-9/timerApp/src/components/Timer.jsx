1e1f1b7ca1fb6c1979666693600ffc14274054cd
Aaidt <aadits2004@gmail.com>
2025-03-10 21:30:01 +0530

    shitty timer done.

--- week-9/timerApp/src/components/Timer.jsx
+++ week-9/timerApp/src/components/Timer.jsx
@@ -4,24 +4,28 @@ import "./Timer.module.css"
 
 const Timer = () => {
 
-  const [Time, setTime] = useState("00");
+  const [Time, setTime] = useState(0);
   const [TimerState, setTimerState] = useState(false);
+  let start;
 
   const startTimer = () => {
-    const start = setInterval(setTime(
-      time => time =+ 1
-    ), 1000);
+    start = setInterval(() => {
+      setTime(time => time + 1)
+    }, 1000);
   }
+  
 
-  const stopTimer = (start) => {
-    clearInterval(start)
+  const stopTimer = () => {
+    setTime(clearInterval(start))
   }
 
 
 
   return (
     <div className="timerApp">
-      Timer
+      <div className="timerCircle">
+        {Time}<br />
+      </div>
       <button onClick={startTimer}>START</button>
       <button onClick={stopTimer}>STOP</button>
     </div>
