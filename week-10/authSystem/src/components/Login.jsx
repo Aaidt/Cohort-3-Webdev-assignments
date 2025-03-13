8e16eeb9ed8ca0c6a34c3da692a9cee7b1b8ad10
Aaidt <aadits2004@gmail.com>
2025-03-13 22:52:34 +0530

    Login page.

--- week-10/authSystem/src/components/Login.jsx
+++ week-10/authSystem/src/components/Login.jsx
@@ -5,7 +5,7 @@ const Login = ({ onLogin: propOnLogin }) => {
   const [username, setUsername] = useState("");
   const contextValue = useContext(AuthContext);
 
-  const handeLogin = () => {
+  const handleLogin = () => {
     if(contextValue?.Login) {
       Login(username);
     }else if(propOnLogin){
@@ -16,8 +16,33 @@ const Login = ({ onLogin: propOnLogin }) => {
 
 
   return (
-    <div>
-
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem', maxWidth: '300px', margin: '0 auto' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}>
+        <label htmlFor="username" style={{ fontWeight: 'bold' }}>Username</label>
+        <input 
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Enter your username: "
+        style={{
+          padding: '0.5rem',
+          borderRadius: '4px',
+          border: '1px solid #ccc'
+        }}
+        />
+        <button 
+          onClick={handleLogin}
+          style={{
+            backgroundColor: '#3f98b5',
+            color: 'white',
+            border: 'none',
+            padding: '0.5rem 1rem',
+            borderRadius: '4px',
+            cursor: 'pointer'
+          }}
+        > LOGIN 
+        </button>
+      </div>
     </div>
   )
 }
