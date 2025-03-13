61ccda7bed148c07f530637545a4327e49804c4b
Aaidt <aadits2004@gmail.com>
2025-03-13 22:04:08 +0530

    cleaned the appbar and gave authsystem the context provider.

--- week-10/authSystem/src/components/AppBar.jsx
+++ week-10/authSystem/src/components/AppBar.jsx
@@ -2,19 +2,9 @@ import React from 'react'
 
 const AppBar = () => {
 
-  const person = "Raman"
-
-  const Login = () => {
-
-  }
   
   return (
     <div>
-      <div className="app-bar">
-        <h2 style={{padding: 15}}><b>AuthSystem Demo</b></h2>
-        <h3>Welcome, {person}!</h3>
-        <button style={{padding: 10, borderRadius: 10}} onClick={Login}>LOGIN</button>
-      </div>
     </div>
   )
 }
--- week-10/authSystem/src/components/AuthSystem.jsx
+++ week-10/authSystem/src/components/AuthSystem.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, createContext, useContext } from 'react'
 import "../App.css"
 import AppBar from "./AppBar"
 
+export const AuthContext = createContext(undefined);
 
 const AuthSystem = () => {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [username, setUsername] = useState("");
   const [useContextApi, setUseContextApi] = useState(false);
+  const [username, setUsername] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const Login = (newUsername) => {
+    setUsername(newUsername);
+    setIsLoggedIn(true);
+  }
+
+  const Logout = () => {
+    setUsername("");
+    setIsLoggedIn(false);
+  }
+
+  const contextValue = useContextApi ? { username, isLoggedIn, Login, Logout } : undefined;
 
   return (
-    <div>
-      <AppBar />
-    </div>
+    <AuthContext.Provider value={contextValue}>
+      <div>
+        <AppBar />
+      </div>
+    </AuthContext.Provider>
   )
 }
 
