533d7ea5603f72a411dc517064557fb9ad548b85
Aaidt <aadits2004@gmail.com>
2025-03-13 22:46:47 +0530

    handle login.

--- week-10/authSystem/src/components/Login.jsx
+++ week-10/authSystem/src/components/Login.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { AuthContext } from "./AuthSystem";
 
-const Login = () => {
+const Login = ({ onLogin: propOnLogin }) => {
+  const [username, setUsername] = useState("");
+  const contextValue = useContext(AuthContext);
+
+  const handeLogin = () => {
+    if(contextValue?.Login) {
+      Login(username);
+    }else if(propOnLogin){
+      propOnLogin(username);
+    }
+    setUsername("");
+  }
+
+
   return (
-    <div>Login</div>
+    <div>
+
+    </div>
   )
 }
 
