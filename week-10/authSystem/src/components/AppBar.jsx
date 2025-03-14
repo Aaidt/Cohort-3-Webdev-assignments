22d0ea0e8fa3bbcdd477dc326b75b85112e29f19
Aaidt <aadits2004@gmail.com>
2025-03-14 16:40:38 +0530

    auth sys header

--- week-10/authSystem/src/components/AppBar.jsx
+++ week-10/authSystem/src/components/AppBar.jsx
@@ -1,11 +1,24 @@
 import React, { useContext } from 'react'
 import { AuthContext } from "./AuthSystem";
 
-const AppBar = ({ username: propUsername, isLoggedIn: propIsLoggedIn, logout: propLogout}) => {
+const AppBar = ({ username: propUsername, isLoggedIn: propIsLoggedIn, Logout: propLogout}) => {
+  const contextValue = useContext(AuthContext);
 
-  
+  const displayUsername = contextValue?.username ?? propUsername;
+  const displayIsLoggedIn = contextValue?.isLoggedIn ?? propIsLoggedIn;
+  const handleLogout = contextValue?.Logout ?? propLogout;
+
+
   return (
-    <div>
+    <div style={{
+      backgroundColor: '#3f98b5',
+      color: 'white',
+      padding: '1rem',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    }}>
+    <h1 style={{ fontSize: '1.25rem', fontWeight: 'bold' }}>Auth System Demo</h1>
     </div>
   )
 }
