8cc29974b64ffb95292721a63a3b9afb3f3c515f
Aaidt <aadits2004@gmail.com>
2025-03-15 16:20:13 +0530

    isEven selectors done.

--- week-11/recoil/src/App.jsx
+++ week-11/recoil/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect, memo } from "react";
 import { useSetRecoilState, useRecoilValue, RecoilRoot, atom } from "recoil";
-import { counterAtom } from "./store/atom/counter.js"
+import { counterAtom, evenSelector } from "./store/atom/counter.js"
 
 // const CountContext = createContext();
 
@@ -14,65 +14,106 @@ import { counterAtom } from "./store/atom/counter.js"
 
 // }
 
-function Counter() {
-  const [count, setCount] = useState(0);
+// function Counter() {
+//   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    setInterval(() => {
-      setCount(count => count+1)
-    }, 3000)
-  }, [])
-  return (
-    <div>
-      <Count />
-      <Increase />
-      <Decrease />
-    </div>
-  )
-}
+//   useEffect(() => {
+//     setInterval(() => {
+//       setCount(count => count+1)
+//     }, 3000)
+//   }, [])
+//   return (
+//     <div>
+//       <Count />
+//       <Increase />
+//       <Decrease />
+//     </div>
+//   )
+// }
 
-const Count = memo(function () {
-  // const { count } = useContext(CountContext);
-  // const count = useRecoilValue(counterAtom)
-  // return <div>{count}</div>
-  return <div>
-    1
-  </div>
-})
-
-
-const Increase = memo(function () {
-  // const { count, setCount } = useContext(CountContext);
-  // const setCount = useSetRecoilState(counterAtom);
-
-  function Increase() {
-    // setCount(c => c + 1)
-  }
-
-  return <button onClick={Increase}>Increase</button>
-})
+// const Count = memo(function () {
+//   // const { count } = useContext(CountContext);
+//   // const count = useRecoilValue(counterAtom)
+//   // return <div>{count}</div>
+//   return <div>
+//     1
+//   </div>
+// })
 
 
-const Decrease = memo(function () {
-  // const { count, setCount } = useContext(CountContext);
-  // const setCount = useSetRecoilState(counterAtom);
+// const Increase = memo(function () {
+//   // const { count, setCount } = useContext(CountContext);
+//   // const setCount = useSetRecoilState(counterAtom);
 
-  function Decrease() {
-    // setCount(c => c - 1);
-  }
-  return <button onClick={Decrease} >Decrease</button>
-})
+//   function Increase() {
+//     // setCount(c => c + 1)
+//   }
+
+//   return <button onClick={Increase}>Increase</button>
+// })
+
+
+// const Decrease = memo(function () {
+//   // const { count, setCount } = useContext(CountContext);
+//   // const setCount = useSetRecoilState(counterAtom);
+
+//   function Decrease() {
+//     // setCount(c => c - 1);
+//   }
+//   return <button onClick={Decrease} >Decrease</button>
+// })
 
 
 function App() {
-  return (
-    <div>
-      <RecoilRoot>
-        <Counter />
-      </RecoilRoot>
-    </div>
-  )
+  // return (
+  //   <div>
+  //     <RecoilRoot>
+  //       <Counter />
+  //     </RecoilRoot>
+  //   </div>
+  // )
+
+  return <div>
+    <RecoilRoot>
+      <Buttons />
+      <Counter />
+      <IsEven />
+    </RecoilRoot>
+
+  </div>
 }
 
+
+function Buttons() {
+  const setCount = useSetRecoilState(counterAtom)
+
+  function increase() {
+    setCount(c => c + 2);
+  }
+
+  function decrease() {
+    setCount(c => c - 1);
+  }
+
+
+  return <div>
+    <button onClick={increase}>Increase</button>
+    <button onClick={decrease}>Decrease</button>
+  </div>
+}
+
+function Counter() {
+  const count = useRecoilValue(counterAtom);
+  return <div>
+    {count}
+  </div>
+}
+
+function IsEven() {
+  const even = useRecoilValue(evenSelector);
+  return <div>
+    {even ? "Even" : "Odd"}
+  </div>
+}
 
 export default App;
