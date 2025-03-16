e16e64f2fad0c1b5c405532466ccdd8a93694fcf
Aaidt <aadits2004@gmail.com>
2025-03-16 15:00:16 +0530

    10 users fetch logic in useEffect.

--- week-10/randUser/randomUser/src/components/randomUser.jsx
+++ week-10/randUser/randomUser/src/components/randomUser.jsx
@@ -2,15 +2,30 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export const RandomUsers = () => {
-    useEffect(async () => {
-
-        const response = await axios.get("https://randomuser.me/api/");
-        const user = response.data.results[0];
-        
-        console.log();
-
-
+    const [user, setUser] = useState([])
+    useEffect(() => {
+        async function fetchUsers() {
+            let fetchedUsers = [];
+            for (let i = 0; i < 10; i++) {
+                const response = await axios.get("https://randomuser.me/api");
+                const data = response.data.results[0];
+                const name = `${data.name.title} ${data.name.first} ${data.name.last}`
+                fetchedUsers.push(name);
+            }
+            setUser(fetchedUsers);
+        }
+        fetchUsers();
     }, [])
-}
 
- 
+    return (
+        <div>
+            {user.length > 0 ? (    
+                user.map((u, index) => (
+                    <div key={index}>{u}</div>
+                ))
+            ) : (
+                <p>Loading....</p>
+            )}
+        </div>
+    )
+}
