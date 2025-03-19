00ed9a9b8071216291f522bf98bc4953b1bb4059
Aaidt <aadits2004@gmail.com>
2025-03-19 09:54:29 +0530

    added Add to cart button and styles.

--- week-11/amazonCart/src/components/WishList.jsx
+++ week-11/amazonCart/src/components/WishList.jsx
@@ -10,16 +10,39 @@ export const WishList = () => {
     // console.log(items)
     const Item = () => {
         return <div>
-            <div>
+            <div
+                style={{
+                    justifyItems: "center",
+                    display: "flex",
+                    margin: 20,
+                    padding: 10
+                }}
+            >
                 {items.map((product) => {
                     return (
                         <div
-                            // style={WishList.module.css}
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                borderRadius: 10,
+                                width: "15vw",
+                                boxShadow: " 0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+                                padding: 20,
+                                margin: 20,
+                                border: "1px solid black"
+                            }}
                             key={product.id}
                         >
-                            <img style={{height:"15vh", width:"10vw"}} src={product.image} />
+                            <img style={{ height: "15vh", width: "10vw" }} src={product.image} />
                             {product.title}<br />
-                            {product.price}
+                            Price: Rs.{product.price}
+                            <button style={{
+                                borderRadius: 5,
+                                margin: 10
+                            }}>
+                                Add to Cart
+                            </button>
                         </div>
                     )
                 })}
