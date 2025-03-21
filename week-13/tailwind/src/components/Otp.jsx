3f02cab173607646f51f203570b04be5ccbe8b33
Aaidt <aadits2004@gmail.com>
2025-03-21 13:32:23 +0530

    iterating through refs for creating multiple subOtpBoxes.

--- week-13/tailwind/src/components/Otp.jsx
+++ week-13/tailwind/src/components/Otp.jsx
@@ -1,40 +1,48 @@
 import { useRef, useState } from "react";
 import { Button } from "./button";
 export const Otp = ({ number }) => {
-    const ref1 = useRef();
-    const ref2 = useRef();
-    const ref3 = useRef();
-    const ref4 = useRef();
-    const ref5 = useRef();
-    const ref6 = useRef();
 
+    const ref = useRef(Array(number).fill(0));
     const [disabled, setDisabled] = useState(true)
 
     return <div className="flex justify-center">
 
-        {Array(number).fill(1).map((x, index) => {
-            <SubOtpBox key={index} onDone={() => {
-                ref2.current.focus();
+        {Array(number).fill(1).map((x, index) =>
+            <SubOtpBox ref={e => ref.current[index] = e} key={index} onDone={() => {
+                if (index + 1 >= number) {
+                    return
+                }
+                ref.current[index + 1].focus();
+            }} goBack={() => {
+                if (index = 0) {
+                    return
+                }
+                ref.current[index - 1].focus();
             }} />
-        })}
-        
+        )}
+
         <Button disabled={disabled}>Sign-up</Button>
     </div>
 
 }
 
 function SubOtpBox({
-    ref, onDone
+    ref, onDone, goBack
 }) {
     const [inputBoxVal, setInputBoxVal] = useState("")
     return <div>
-        <input value={inputBoxVal} ref={ref} onChange={(e) => {
-            const val = e.target.value;
-            if (val == "1" || val == "2" || val == "3" || val == "4" || val == "5" || val == "6" || val == "7" || val == "8" || val == "9") {
-                setInputBoxVal(val);
-                onDone()
+        <input value={inputBoxVal} ref={ref} onKeyUp={(e) => {
+            if (e.key == "Backspace") {
+                goBack();
             }
-        }} type="text" className="text-white px-4 outline-none w-[40px] h-[50px] bg-blue-500 m-1 rounded-xl"></input>
+        }}
+            onChange={(e) => {
+                const val = e.target.value;
+                if (val == "1" || val == "2" || val == "3" || val == "4" || val == "5" || val == "6" || val == "7" || val == "8" || val == "9") {
+                    setInputBoxVal(val);
+                    onDone()
+                }
+            }} type="text" className="text-white px-4 outline-none w-[40px] h-[50px] bg-blue-500 m-1 rounded-xl"></input>
     </div>
 
 }
