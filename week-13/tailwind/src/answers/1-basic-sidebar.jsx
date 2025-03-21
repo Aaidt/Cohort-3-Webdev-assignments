86a5506a6714bda2c43e7f8e08db952e9490bba6
Aaidt <aadits2004@gmail.com>
2025-03-21 15:42:14 +0530

    sidebar hidden logic.

--- week-13/tailwind/src/answers/1-basic-sidebar.jsx
+++ week-13/tailwind/src/answers/1-basic-sidebar.jsx
@@ -1,10 +1,10 @@
 export function SideBar(){
     return <div className="flex">
-        <div className="bg-blue-200 h-screen w-[200px]">
+        <div className="bg-blue-200 h-screen w-64 hidden md:block">
             Sidebar
         </div>
 
-        <div className="bg-red-200 h-auto w-screen">
+        <div className="bg-red-200 h-screen w-screen">
             Content
         </div>
     </div>
