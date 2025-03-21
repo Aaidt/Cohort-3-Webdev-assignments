fc39cb08368621558814794b6c241548fc1193b4
Aaidt <aadits2004@gmail.com>
2025-03-21 11:16:52 +0530

    updated colors to code the fe

--- week-13/tailwind/src/App.jsx
+++ week-13/tailwind/src/App.jsx
@@ -5,11 +5,13 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-blue-300">
-      Hello world!
-    </h1>
-    </>
+    <div className="h-screen bg-blue-900">
+      <div className="h-20 w-20 object-cover flex font-bold">
+        <img className="object-contain" src="https://imgs.search.brave.com/U0H7yjSthhXgU6kCV3sqL74tT9F2Hbf_1xqYYU8pgpI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9kZXNrdG9wLWNv/bXB1dGVyLWtleWJv/YXJkLWJhY2tncm91/bmRfNDg4MjIwLTU0/MzM1LmpwZz9zZW10/PWFpc19oeWJyaWQ" />
+        <div>Webinar.gg</div>
+      </div>
+
+    </div>
   )
 }
 
--- week-13/tailwind/tailwind.config.js
+++ week-13/tailwind/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          200: "#7589a0",
+          500: "#19385f",
+          700: "#00274e" 
+        },
+        green: {
+          200: "#3095a0"
+        }
+      }
+    },
   },
   plugins: [],
 }
