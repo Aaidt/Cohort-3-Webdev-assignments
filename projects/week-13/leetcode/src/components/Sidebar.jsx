aa7f9b3d3741745d260dc85d2fcff903a7078f00
Aaidt <aadits2004@gmail.com>
2025-03-22 20:25:21 +0530

    sidebar (toggalable) done.

---
+++ projects/week-13/leetcode/src/components/Sidebar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export const Sidebar = () => {
+    const [sidebarOpen, setSidebarOpen] = useState(true)
+    return (
+        <div>
+            {sidebarOpen ? (
+                <div className="w-2xs bg-gray-800 h-screen" >
+                    <svg
+                        onClick={() => setSidebarOpen(!sidebarOpen)}
+                        id="icon"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="text-white size-6 cursor-pointer transition-all duration-5000">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
+                    </svg>
+                </div >
+            ) : (
+                <div>
+                    <svg
+                        onClick={() => setSidebarOpen(!sidebarOpen)}
+                        id="icon"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="text-white size-6 cursor-pointer transition-all duration-5000">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
+                    </svg>
+                </div>
+            )
+            }
+        </div>
+    )
+} 
