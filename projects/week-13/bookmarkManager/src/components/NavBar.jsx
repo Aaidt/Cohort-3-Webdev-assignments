11e221f5f58de416e4246057db54c3fdc10941bf
Aaidt <aadits2004@gmail.com>
2025-03-22 12:13:45 +0530

    tailwind conffiguration done. finally.

--- projects/week-13/bookmarkManager/src/components/NavBar.jsx
+++ projects/week-13/bookmarkManager/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const NavBar = () => {
     return (
-        <div className="bg-blue-800">
+        <div className="bg-green-200 h-screen">
             Bookamark
         </div>
     )
