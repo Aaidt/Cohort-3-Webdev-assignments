a67bd3fc47e3bf9f4c0946490aeaec49e059862e
Aaidt <aadits2004@gmail.com>
2025-03-22 21:01:22 +0530

    card

--- projects/week-13/leetcode/src/components/Card.jsx
+++ projects/week-13/leetcode/src/components/Card.jsx
@@ -7,7 +7,7 @@ export const Card = () => {
             <div className="bg-white w-20 h-20 rounded-lg">
                 <img className="w-12 h-12 translate-3.5" src="https://uxwing.com/wp-content/themes/uxwing/download/arts-graphic-shapes/star-symbol-icon.svg" />
             </div>
-            <div className="font-bold text-white text-lg">Favourite</div>
+            <div className="font-bold text-white text-xl translate-1">Favourite</div>
         </div>
     )
 } 
