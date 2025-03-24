d7c2ddc813061fe12fb7fef0fdcbc74f41f11a11
Aaidt <aadits2004@gmail.com>
2025-03-24 21:37:56 +0530

    Exclude<>

--- projects/week-14/typescript/src/index.ts
+++ projects/week-14/typescript/src/index.ts
@@ -1,233 +1,233 @@
-// let x: number = 1
-// console.log(x);
+let x: number = 1
+console.log(x);
 
-// function greet(firstName: string | number) {
-//     console.log(`Hello ${firstName}`);
-// }
-// greet(1);
-// greet("aadit");
+function greet(firstName: string | number) {
+    console.log(`Hello ${firstName}`);
+}
+greet(1);
+greet("aadit");
 
-// function sum(a: number, b: number): number {
-//     return a + b
-// }
+function sum(a: number, b: number): number {
+    return a + b
+}
 
-// let ans = sum(1,2);
-// console.log(ans);
+let ans = sum(1,2);
+console.log(ans);
 
-// function isLegal(age: number){
-//     if(age>18){
-//         return true
-//     }else{
-//         return false
-//     }
-// }
-// console.log(isLegal(19))
+function isLegal(age: number){
+    if(age>18){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(isLegal(19))
 
-// function delayedCall(fn: (() => string) | ((a: number) => number)){
-//     setTimeout(fn, 1000);
-// }
+function delayedCall(fn: (() => string) | ((a: number) => number)){
+    setTimeout(fn, 1000);
+}
 
-// delayedCall(function(){
-//     console.log("hello");
-//     return 1;
-// })
+delayedCall(function(){
+    console.log("hello");
+    return 1;
+})
 
-// interface UserType {
-//     name: string,
-//     age: number
-// }
+interface UserType {
+    name: string,
+    age: number
+}
 
-// function hello(user: UserType){
-//     console.log("hello" + user.age)
-// }
-// hello({
-//     name:"aadit",
-//     age: 21
-// })
+function hello(user: UserType){
+    console.log("hello" + user.age)
+}
+hello({
+    name:"aadit",
+    age: 21
+})
 
-// interface Address {
-//     address?: {
-//         city: string,
-//         pincode: number
-//     }
-// }
+interface Address {
+    address?: {
+        city: string,
+        pincode: number
+    }
+}
 
-// interface User {
-//     name: string,
-//     age: number,
-//     address?: Address
-// }
+interface User {
+    name: string,
+    age: number,
+    address?: Address
+}
 
-// let user2: User =  {
-//     name: "adsa",
-//     age: 43
-// }
+let user2: User =  {
+    name: "adsa",
+    age: 43
+}
 
-// let user: User = {
-//     name: "aadit",
-//     age: 21,
-//     address: {
-//         city: "thane",
-//         pincode: 898934
-//     }
-// }
+let user: User = {
+    name: "aadit",
+    age: 21,
+    address: {
+        city: "thane",
+        pincode: 898934
+    }
+}
 
-// function isLegal(user: User): boolean {
-//     return user.age > 18;
-// }
+function isLegal(user: User): boolean {
+    return user.age > 18;
+}
 
-// console.log(isLegal(user))  
+console.log(isLegal(user))  
 
-// interface Person {
-//     name: string,
-//     age: number,
-//     isLegal(): boolean
-//     // greet: () => string
-// }
+interface Person {
+    name: string,
+    age: number,
+    isLegal(): boolean
+    // greet: () => string
+}
 
-// let people: Person = {
-//     name: "adsd",
-//     age: 23,
+let people: Person = {
+    name: "adsd",
+    age: 23,
 
-//     // greet: () => {
-//     //     return "hi"
-//     // }
-// }
-// const ans = people.greet()  
-// console.log(ans)
+    // greet: () => {
+    //     return "hi"
+    // }
+}
+const ans = people.greet()  
+console.log(ans)
 
-// class Manager implements Person {
-//     name: string;
-//     age: number;
-//     constructor(name: string, age: number) {
-//         this.name = name;
-//         this.age = age;
-//     }
-//     isLegal(){
-//         return this.age > 18
-//     }
-// }
+class Manager implements Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    isLegal(){
+        return this.age > 18
+    }
+}
 
-// let user = new  Manager("John", 57);
-// console.log(user.isLegal())
+let user = new  Manager("John", 57);
+console.log(user.isLegal())
 
-// abstract class User {
-//     name: string
-//     constructor(name: string) {
-//         this.name = name
-//     }
-//     abstract greet(): string
-//     hello(){
-//         console.log("hi there")
-//     }
-// }
+abstract class User {
+    name: string
+    constructor(name: string) {
+        this.name = name
+    }
+    abstract greet(): string
+    hello(){
+        console.log("hi there")
+    }
+}
 
-// class Employee extends User{
-//     name: string;
-//     constructor(name: string){
-//         super(name)
-//         this.name = name;
-//     }
-//     greet(){
-//         console.log("hi" + this.name)
-//     }
-// }
+class Employee extends User{
+    name: string;
+    constructor(name: string){
+        super(name)
+        this.name = name;
+    }
+    greet(){
+        console.log("hi" + this.name)
+    }
+}
 
 
-// type Employee = {
-//     name: string,
-//     startDate: number    
-// }
+type Employee = {
+    name: string,
+    startDate: number    
+}
 
-// type Manager = {
-//     name: string,
-//     department: string
-// }
+type Manager = {
+    name: string,
+    department: string
+}
 
-// // type TeamLead = Employee & Manager;
+// type TeamLead = Employee & Manager;
 
-// type TeamLead = Employee | Manager
+type TeamLead = Employee | Manager
 
-// let e: Employee = {
-//     name: "adsfsd",
-//     startDate: 43243243
-// }
+let e: Employee = {
+    name: "adsfsd",
+    startDate: 43243243
+}
 
-// let m: Manager = {
-//     name: "sfasd",
-//     department: "sfdfd"
-// }
+let m: Manager = {
+    name: "sfasd",
+    department: "sfdfd"
+}
 
-// let t: TeamLead = {
-//     name: "fsdf",
-//     startDate: 4234,
-//     department: "das"
-// }
+let t: TeamLead = {
+    name: "fsdf",
+    startDate: 4234,
+    department: "das"
+}
 
-// interface User{
-//     name: string,
-//     age: number
-// }
+interface User{
+    name: string,
+    age: number
+}
 
-// function sumOfAge (user1: User, user2: User): number {
-//     return user1.age + user2.age
-// }
+function sumOfAge (user1: User, user2: User): number {
+    return user1.age + user2.age
+}
 
-// const ans = sumOfAge({name: "aadit", age: 21}, {name: "smth", age:23});
-// console.log(ans)
+const ans = sumOfAge({name: "aadit", age: 21}, {name: "smth", age:23});
+console.log(ans)
 
-// interface User {
-//     name: string,
-//     age: number,
-//     email: string,
-//     password: string
-// }
+interface User {
+    name: string,
+    age: number,
+    email: string,
+    password: string
+}
 
-// type updateProp = Pick<User, "name" | "age" | "password">
+type updateProp = Pick<User, "name" | "age" | "password">
 
-// type updatePropPartial = Partial<updateProp>
+type updatePropPartial = Partial<updateProp>
 
-// function updateUser(updatedProps: updatePropPartial){
-//     console.log(`${updatedProps.age}`)  
-// }
+function updateUser(updatedProps: updatePropPartial){
+    console.log(`${updatedProps.age}`)  
+}
 
-// updateUser({
-//     name: "aadit"
-// })
+updateUser({
+    name: "aadit"
+})
 
-// type User = {
-//     name: string,
-//     age: number
-// }
+type User = {
+    name: string,
+    age: number
+}
 
-// const user: Readonly<User> = {
-//     name: "aadsa",
-//     age: 23
-// }
+const user: Readonly<User> = {
+    name: "aadsa",
+    age: 23
+}
 
-// user.age  = 45
+user.age  = 45
 
-// const User = {
-//     "idd": {
-//         name: "aasd",
-//         age: 23
-//     },
-//     "idd2": {
-//         name: "aasd",
-//         age: 23
-//     }
-// }
+const User = {
+    "idd": {
+        name: "aasd",
+        age: 23
+    },
+    "idd2": {
+        name: "aasd",
+        age: 23
+    }
+}
 
-// type Users = Record<string, {name: string, age: number}>
+type Users = Record<string, {name: string, age: number}>
 
-// type User = {
-//     name: string,
-//     age: number
-// }
+type User = {
+    name: string,
+    age: number
+}
 
-// type Users = {
-//     [key: string]: User
-// }
+type Users = {
+    [key: string]: User
+}
 
 const users = new Map<string, {name: string, age: number}>();
 users.set("user1", {
@@ -238,3 +238,6 @@ users.set("user2", {
 })
 
 const user = users.get("user1");
+
+type evenType = "click" | "scroll" | "mouse";
+type eventTypeExclude = Exclude<evenType, "scroll">
