641ebb0b0bd4e3be51ad63f8274aba182dc1406f
Aaidt <aadits2004@gmail.com>
2025-03-26 09:29:13 +0530

    content and links db schema

--- projects/brainly/src/db/db.ts
+++ projects/brainly/src/db/db.ts
@@ -6,3 +6,20 @@ const UserSchema = new Schema({
 });
 
 export const UserModel = model("users", UserSchema);
+
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{ types: mongoose.Types.ObjectId, ref: 'Tag' }],
+    type: String,
+    userId: { type: mongoose.Types.ObjectId, ref: 'users', required: true}
+})
+
+
+const LinkSchema = new Schema({
+    hash: String,
+    userId: { type: mongoose.Types.ObjectId, ref: 'users', required: true}
+})
+
+export const ContentModel = model("content", ContentSchema);
+export const LinkModel = model("links", LinkSchema);
