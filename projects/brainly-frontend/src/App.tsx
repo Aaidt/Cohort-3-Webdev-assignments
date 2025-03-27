9263ecd9768f870cd6604f084200d47a2278305a
Aaidt <aadits2004@gmail.com>
2025-03-27 10:05:00 +0530

    Card component done.

--- projects/brainly-frontend/src/App.tsx
+++ projects/brainly-frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import React from "react";
 import "./App.css"
 import { Button } from "./components/ui/Button"
 import { PlusIcon } from "./components/icons/PlusIcon"
 import { ShareIcon } from "./components/icons/ShareIcon"
+import { Card } from "./components/ui/Card"
 
 function App() {
   return (
     <div>
-      <Button onClick={() => {}} startIcon={<PlusIcon size={"lg"} />} variant="primary" text="Add content" size="md" />
-      <Button onClick={() => {}} startIcon={<ShareIcon size={"md"} />} variant="secondary" text="Share brain" size="md" />
+      <Button onClick={() => { }} startIcon={<PlusIcon size={"lg"} />} variant="primary" text="Add content" size="md" />
+      <Button onClick={() => { }} startIcon={<ShareIcon size={"md"} />} variant="secondary" text="Share brain" size="md" />
+
+      <div className="flex">
+        <Card type="youtube" title="yt ideas" link="https://www.youtube.com/embed/ezuyuoP5OI8?si=bQCu8wfe-QWJiRvg" />
+        <Card type="twitter" title="tweet ideas" link="https://x.com/danghentschel/status/1905082439204253775" />
+      </div>
     </div>
   )
 }
---
+++ projects/brainly-frontend/src/components/ui/Card.tsx
@@ -0,0 +1,51 @@
+import { ShareIcon } from "../icons/ShareIcon"
+
+
+interface CardProps {
+    title: string,
+    type: "youtube" | "twitter",
+    link: string
+}
+
+export const Card = ({ title, type, link }: CardProps) => {
+    return <>
+        <div className="bg-white rounded-md p-4 border-gray-200 max-w-72 border shadow-lg">
+            <div className="flex justify-between">
+                <div className="flex items-center">
+                    <div className="text-gray-500 pr-2">
+                        <a href={link} target="_blank">
+                            <ShareIcon size="md" />
+                        </a>
+                    </div>
+                    {title}
+                </div>
+                <div className="flex text-gray-500">
+                    <div className="pr-2 ">
+                        <ShareIcon size="md" />
+                    </div>
+                    <div>
+                        <ShareIcon size="md" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="pt-4">
+                {type === "youtube" && <iframe
+                    className="w-full"
+                    src={link.replace("watch", "embed")}
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen>
+                </iframe>}
+
+                {type === "twitter" && <blockquote className="twitter-tweet">
+                    <a href={link.replace("x.com", "twitter.com")}></a>
+                </blockquote>}
+            </div>
+
+
+        </div>
+    </>
+}   
