0677d895ff8326b1678e9d2d5ccda18abdc56600
Aaidt <aadits2004@gmail.com>
2025-03-27 14:26:17 +0530

    icons styling

--- projects/brainly-frontend/src/components/ui/SideBar.tsx
+++ projects/brainly-frontend/src/components/ui/SideBar.tsx
@@ -1,7 +1,19 @@
+import { SideBarItems } from "./SideBarItems"
+import { YoutubeIcon } from "../icons/YoutubeIcon"
+import { TwitterIcon } from "../icons/TwitterIcon"
 
 
 export const SideBar = () => {
-        return <div className="h-screen bg-slate-100 border-r border-gray-400 w-72 absolute left-0 top-0 slate-500 border-2">
-        hey
+    return <div className="h-screen bg-slate-100 pl-4 border-r border-gray-400 w-72 absolute left-0 top-0 slate-500 border-2">
+        <div className="text-2xl flex pt-4">
+            <div>
+                
+            </div>
+            BRAINLY
+        </div>
+        <div className="pt-4 text-gray-500">
+            <SideBarItems icons={<TwitterIcon />} text="Twitter" />
+            <SideBarItems icons={<YoutubeIcon />} text="YouTube" />
+        </div>
     </div>
 }
--- projects/brainly-frontend/src/components/ui/SideBarItems.tsx
+++ projects/brainly-frontend/src/components/ui/SideBarItems.tsx
@@ -1,10 +1,16 @@
 
 
-export const SideBarItems = ({text, icons}: {
+export const SideBarItems = ({ text, icons }: {
     text: string,
     icons: React.ReactElement
 }) => {
     return <div className="flex">
-        {icons} {text}
+        <div className="p-3 text-gray-500">
+            {icons}
+        </div>
+        <div className="p-3 text-gray-500">
+            {text}
+        </div>
+
     </div>
 }
